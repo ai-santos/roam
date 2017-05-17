8d389c643ae5a21deb870cdd12428ff8f09738b1
Aileen <asantos3026@gmail.com>
2017-05-17 15:02:47 -0700

    WIP Auth process started

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import index from './routes/index'
 import path from 'path' //package like express
 import logger from 'morgan' // http request logger middlewre for node.js. it's a helper that collects log from your server
 import bodyParser from 'body-parser' //allows to have access to the body of the request using request.body. u can do it urself but body parser does it for u
+import cookieParser from 'cookie-parser'
 
 const server = express() //how u wire up express . u can access this express funciton using const server
 
@@ -13,6 +14,7 @@ server.set('view engine', 'ejs');
 
 server.use(logger('dev'))
 server.use(express.static(path.join(__dirname + '/public')))
+server.use(cookieParser())
 server.use(bodyParser.urlencoded({extended: true}));
 
 //routes
