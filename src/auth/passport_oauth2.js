84bfe46c6647c80c70e3947be79b018510f23795
Aileen <asantos3026@gmail.com>
2017-05-16 19:41:15 -0700

    WIP Add passport facebook to application

---
+++ src/auth/passport_oauth2.js
@@ -0,0 +1,15 @@
+var passport = require('passport')
+
+passport.use(new OAuth2Strategy({
+    authorizationURL: 'https://www.example.com/oauth2/authorize',
+    tokenURL: 'https://www.example.com/oauth2/token',
+    clientID: 'nB2YOUc01kBmilAqJABpNd6aOyFzjyF8',
+    clientSecret: 'VHfmKyD2kTlzewWw_jDDR5KhRzkP_RZFx_nR5Jyxs-xJmqxcVH334AVH74poZtZB',
+    callbackURL: "http://127.0.0.1:8080/auth_callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
