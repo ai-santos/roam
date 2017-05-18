0584399ad4c7d14afa326229fcc1bf220779ba9b
Aileen <asantos3026@gmail.com>
2017-05-18 11:00:30 -0700

    WIP does not redirect to dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,15 @@
 import express from 'express'
 
+const authUrl = "https://github.com/login/oauth/authorize"
 const router = express.Router()
 
 router.get( '/', (request, response, next) => {
   response.render('home')
 })
 
-router.get( '/dashboard', (request, response, next) => {
-  response.render('users/dashboard')
-})
 
 router.get( '/cities', (request, response, next) => {
   response.render('cities')
 })
-
 
 module.exports = router
